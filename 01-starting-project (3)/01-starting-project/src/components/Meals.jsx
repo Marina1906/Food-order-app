7c7f9a4233f4aa9a4ff4d67fe2ccf05c45228d0c
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-01-27 00:20:59 +0100

    edited code

---
+++ 01-starting-project (3)/01-starting-project/src/components/Meals.jsx
@@ -0,0 +1,14 @@
+export default function Meals () {
+    async function fetchMeals (){
+        
+   const response = await fetch('http://localhost:3000/meals');
+        
+   if (!response.ok){
+
+   }
+   
+
+}
+
+    return <ul id="meals"></ul>
+}
