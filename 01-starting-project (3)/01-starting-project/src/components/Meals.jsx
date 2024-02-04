b74a2ed0bb4586117a9f465e37098bd25c8010dd
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-04 14:16:09 +0100

    edited code

--- 01-starting-project (3)/01-starting-project/src/components/Meals.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/Meals.jsx
@@ -1,26 +1,30 @@
-import {useState, useEffect} from 'react';
-
+import { useState, useEffect } from 'react';
 import MealItem from './MealItem.jsx';
 
-export default function Meals () {
-  const [loadedMeals, setLoadedMeals] = useState ({});
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
 
-  useEffect (() => {
-    async function fetchMeals () {
-      const response = await fetch ('http://localhost:3000/meals');
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch('http://localhost:3000/meals');
 
       if (!response.ok) {
+        // Handle error here
       }
-      const meals = await response.json ();
-      setLoadedMeals (meals);
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
     }
 
-    fetchMeals ();
+    fetchMeals();
   }, []);
 
   return (
     <ul id="meals">
-      {loadedMeals.map (meal => <MealItem key={meal.id} meal={meal} />)}
+      {loadedMeals.map(meal => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
     </ul>
   );
 }
+
