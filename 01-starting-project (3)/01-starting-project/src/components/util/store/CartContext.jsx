21a3e8382b76b675ce978359fceeb9d341113f76
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-15 19:45:32 +0100

    Update CartContext.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
     items: [],
@@ -6,7 +6,25 @@ const CartContext = createContext({
     removeItem: (id) => {}
 });
 
+function cartReducer (state, action) {
+    if (action.type==='ADD_ITEM') {
+        const existingCartItemIndex = state.items.findIndex (
+            (item)=> item.id === action.item.id
+            );
+
+            
+    }
+
+    if (action.type ==='REMOVE_ITEM') {
+      
+    }
+
+    return state;
+}
+
 export function CartContextProvider ({children}) {
+    useReducer(cartReducer, {items: []});
+
     return <CartContext.Provider>{children}</CartContext.Provider>;
 }
 export default CartContext;
