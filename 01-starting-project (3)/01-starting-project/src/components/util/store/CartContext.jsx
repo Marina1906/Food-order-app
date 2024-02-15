89cba9db7598cc5532aefbed19efef34ec58f7d8
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-15 20:04:10 +0100

    Update CartContext.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
@@ -1,41 +1,40 @@
-import { createContext, useReducer } from "react";
+import {createContext, useReducer} from 'react';
 
-const CartContext = createContext({
-    items: [],
-    addItem: (item) => {},
-    removeItem: (id) => {}
+const CartContext = createContext ({
+  items: [],
+  addItem: item => {},
+  removeItem: id => {},
 });
 
 function cartReducer (state, action) {
-    if (action.type==='ADD_ITEM') {
-        const existingCartItemIndex = state.items.findIndex (
-            (item)=> item.id === action.item.id
-            );
+  if (action.type === 'ADD_ITEM') {
+    const existingCartItemIndex = state.items.findIndex (
+      item => item.id === action.item.id
+    );
 
-            const updatedItems = [...state.items];
+    const updatedItems = [...state.items];
 
-            if (existingCartItemIndex > -1) {
-                const existingItem = state.items [existingCartItemIndex]
-               const updatedItem = {
-                ...existingItem,
-                quantity: existingItem.quantity + 1
-               };
-
-            }else {
-                updatedItems.push (action.item);
-            }
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push ({...action.item, quantity: 1});
     }
+  }
 
-    if (action.type ==='REMOVE_ITEM') {
-      
-    }
+  if (action.type === 'REMOVE_ITEM') {
+  }
 
-    return state;
+  return state;
 }
 
-export function CartContextProvider ({children}) {
-    useReducer(cartReducer, {items: []});
+export function CartContextProvider({children}) {
+  useReducer (cartReducer, {items: []});
 
-    return <CartContext.Provider>{children}</CartContext.Provider>;
+  return <CartContext.Provider>{children}</CartContext.Provider>;
 }
 export default CartContext;
