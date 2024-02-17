90f9749c5524888cd837d523658791aeaaca76d7
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-17 19:15:46 +0100

    Update App.jsx

--- 01-starting-project (3)/01-starting-project/src/App.jsx
+++ 01-starting-project (3)/01-starting-project/src/App.jsx
@@ -1,13 +1,12 @@
 import Header from './components/header.jsx';
-
 import Meals from './components/Meals.jsx';
-
+import { CartContextProvider } from './components/util/store/CartContext.jsx';
 function App() {
   return (
-    <>
+    <CartContextProvider>
     <Header/>
   <Meals/>
-    </>
+    </CartContextProvider>
   );
 }
 export default App;
