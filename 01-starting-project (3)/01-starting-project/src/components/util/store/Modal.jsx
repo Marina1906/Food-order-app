16be45b5ed03658f7d19314c279eb0168f48d946
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-17 22:43:31 +0100

    Update Modal.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Modal.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Modal.jsx
@@ -1,6 +1,6 @@
 import {createPortal} from 'react-dom';
 
-export default function Modal ({children}) {
-    return createPortal (<dialog>{children}</dialog>, document.getElementById ('modal')
+export default function Modal ({children, open}) {
+    return createPortal (<dialog open ={open}>{children}</dialog>, document.getElementById ('modal')
     );
 }
