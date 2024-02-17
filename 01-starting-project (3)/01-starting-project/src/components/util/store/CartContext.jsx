bb9b99ef75e33d16c45c6f025723eb96963b2b85
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-17 18:03:26 +0100

    Update CartContext.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
@@ -30,11 +30,12 @@ function cartReducer (state, action) {
 
   if (action.type === 'REMOVE_ITEM') {
     const existingCartItemIndex = state.items.findIndex (
-      item => item.id === action.item.id
+      (item) => item.id === action.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
     const updatedItems = [...state.items];
+
     if (existingCartItem.quantity === 1) {
       updatedItems.splice (existingCartItemIndex, 1);
     } else {
@@ -53,14 +54,21 @@ function cartReducer (state, action) {
 export function CartContextProvider({children}) {
   const [cart, dispatchCartAction] =useReducer (cartReducer, {items: []});
 
+
+  function addItem (item) {
+    dispatchCartAction ({type: 'ADD_ITEM', item});
+  }
+
+  function removeItem (id) {
+    dispatchCartAction ({type: 'REMOVE_ITEM', id});
+  }
+
   const cartContext =
   {
-    items: cart.items
+    items: cart.items,
+    addItem,
+    removeItem
   };
-
-  function addItem (item) {}
-
-  functionremoveItem (id) {}
 
   return <CartContext.Provider vale = {CartContext}>{children}</CartContext.Provider>;
 }
