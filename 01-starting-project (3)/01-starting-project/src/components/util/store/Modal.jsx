28ee1ac23e1193b35cf2aa50ddba9e65244fc4eb
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-17 22:55:24 +0100

    Update Modal.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Modal.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Modal.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import {createPortal} from 'react-dom';
 
-export default function Modal ({children, open}) {
-    return createPortal (<dialog open ={open}>{children}</dialog>, document.getElementById ('modal')
+export default function Modal ({children, open, className = ''}) {
+const dialog = useRef ();
+
+    useEffect (() => {
+        if (open) {
+            dialog.current.showModal ();
+        }
+    }, [open]);
+
+    return createPortal (
+    <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+     document.getElementById ('modal')
     );
 }
