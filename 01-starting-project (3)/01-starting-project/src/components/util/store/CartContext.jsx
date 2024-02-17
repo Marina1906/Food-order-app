22f7d29af4b944f6d4bf1cab42f3317b55f53c42
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-17 22:01:43 +0100

    Update CartContext.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/CartContext.jsx
@@ -30,7 +30,7 @@ function cartReducer (state, action) {
 
   if (action.type === 'REMOVE_ITEM') {
     const existingCartItemIndex = state.items.findIndex (
-      (item) => item.id === action.id
+      item => item.id === action.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
@@ -45,15 +45,14 @@ function cartReducer (state, action) {
       };
       updatedItems[existingCartItemIndex] = updatedItem;
     }
-    return {...state, items:updatedItems};
+    return {...state, items: updatedItems};
   }
 
   return state;
 }
 
 export function CartContextProvider({children}) {
-  const [cart, dispatchCartAction] =useReducer (cartReducer, {items: []});
-
+  const [cart, dispatchCartAction] = useReducer (cartReducer, {items: []});
 
   function addItem (item) {
     dispatchCartAction ({type: 'ADD_ITEM', item});
@@ -63,20 +62,15 @@ export function CartContextProvider({children}) {
     dispatchCartAction ({type: 'REMOVE_ITEM', id});
   }
 
-  const cartContext =
-  {
-    items:cart.items,
+  const cartContext = {
+    items: cart.items,
     addItem,
-    removeItem
+    removeItem,
   };
 
-
-
   return (
-  <CartContext.Provider value = {cartContext}>{children}</CartContext.Provider>
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
 }
-
-
 
 export default CartContext;
