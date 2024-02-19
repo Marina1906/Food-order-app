12adb3c41376b9e137098348074c57fd5b36a668
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-19 21:05:36 +0100

    Update Cart.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Cart.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Cart.jsx
@@ -1,9 +1,15 @@
-import Modal from "./Modal";
+import {useContext} from 'react';
+
+import Modal from './Modal';
+import CartContext from './CartContext.jsx';
 
 export default function Cart () {
-    return (
+  useContext (CartContext);
+
+  return (
     <Modal className="cart">
-        <h2>Your Cart</h2>
+      <h2>Your Cart</h2>
+      <ul />
     </Modal>
-    );
+  );
 }
