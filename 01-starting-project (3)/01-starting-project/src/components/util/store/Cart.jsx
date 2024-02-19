04eab191a2c4bdc725f8895ba24ee451cb171efd
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-19 21:12:58 +0100

    Update Cart.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Cart.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Cart.jsx
@@ -4,12 +4,18 @@ import Modal from './Modal';
 import CartContext from './CartContext.jsx';
 
 export default function Cart () {
-  useContext (CartContext);
+  const cartCtx = useContext (CartContext);
 
   return (
     <Modal className="cart">
       <h2>Your Cart</h2>
-      <ul />
-    </Modal>
+      <ul>
+        {cartCtx.Ctx.items.map ((item) => (
+        <li key= {item.id}>
+            {item.name} - {item.quantity}
+        </li>
+        ))}
+      </ul>
+      </Modal>
   );
-}
+        }
