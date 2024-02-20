fba7cf2a0ab80e5b22a1b557f1cea7ffe63eb949
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-20 15:26:06 +0100

    Update CartItem.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/CartItem.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/CartItem.jsx
@@ -1,15 +1,23 @@
-import { currencyFormatter } from "../formatting.js";
+import {currencyFormatter} from '../formatting.js';
 
-export default function CartItem ({name, quantity, price}) {
-    return ( <li className="cart-item">
-        <p>{name} - {quantity} x {currencyFormatter.format (price)}
-        </p>
-        <p className="cart-item-actions">
-            
-<button>-</button>
-<span>{quantity}</span>
-<button>+</button>
-        </p>
+export default function CartItem({
+  name,
+  quantity,
+  price,
+  onIncreasee,
+  onDecrease,
+}) {
+  return (
+    <li className="cart-item">
+      <p>
+        {name} - {quantity} x {currencyFormatter.format (price)}
+      </p>
+      <p className="cart-item-actions">
+
+        <button onClick ={onDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick= {onIncreasee}>+</button>
+      </p>
     </li>
-    );
+  );
 }
