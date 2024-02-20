614dcf4e698c956f3ee718eb33a5309a72617500
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-20 15:07:55 +0100

    Update CartItem.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/CartItem.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/CartItem.jsx
@@ -1,10 +1,13 @@
-export default function CartItem () {
+export default function CartItem ({name, quantity, price}) {
     return <li className="cart-item">
         <p>
             NAME - QTY x PRICE
         </p>
-        <p>
-
+        <p className="cart-item-actions">
+            
+<button>-</button>
+<span>QTY</span>
+<button>+</button>
         </p>
     </li>
 }
