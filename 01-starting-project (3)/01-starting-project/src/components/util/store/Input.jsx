1d391b8fa65ce1be53f2662cc62de6f4d1a3da59
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-21 21:21:59 +0100

    Update Input.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Input.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Input.jsx
@@ -1,6 +1,8 @@
 export default function input ({label, id}) {
-    return <p className="control">
+    return(<p className="control">
         <label htmlFor={id}>{label}</label>
-        <input id={id} name = {id} required/>
+        <input id={id} name = {id} required {...props}/>
     </p>
+
+    );
 }
