ce71ba23f611d375f8f681dcc48cd1578524c799
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-21 22:18:09 +0100

    Update Checkout.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
@@ -9,7 +9,7 @@ import UserProgressContext from './UserProgressContext.jsx';
 
 export default function Checkout () {
   const cartCtx = useContext (CartContext);
- const userProgressCtx = useContext (UserProgressContext);
+  const userProgressCtx = useContext (UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce (
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -23,11 +23,12 @@ export default function Checkout () {
   function handleSubmit (event) {
     event.preventDefault ();
 
-    new FormData (event.target);
+    const fd = new FormData (event.target);
+    const customerData = Object.fromEntries (fd.entries());
   }
 
   return (
-    <Modal open = {userProgressCtx.progress ==='checkout'} onClose={handleClose}>
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format (cartTotal)}</p>
@@ -40,7 +41,7 @@ export default function Checkout () {
           <Input label="City" type="text" id="city" />
         </div>
         <p className="modal-actions">
-          <Button type="button"textOnly onClick={handleClose}>Close</Button>
+          <Button type="button" textOnly onClick={handleClose}>Close</Button>
           <Button textOnly>Submit Order</Button>
         </p>
       </form>
