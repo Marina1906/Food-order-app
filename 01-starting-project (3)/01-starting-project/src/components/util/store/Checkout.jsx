842eb9d2220ee7234884e9f0b6e351947e6ad9d3
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-21 21:39:15 +0100

    Update Checkout.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
@@ -1,32 +1,36 @@
-import { useContext } from "react";
+import {useContext} from 'react';
 
-import Modal from "./Modal.jsx";
-import CartContext from "./CartContext.jsx";
-import { currencyFormatter } from "../formatting.js";
-import input from "./Input.jsx";
+import Modal from './Modal.jsx';
+import CartContext from './CartContext.jsx';
+import {currencyFormatter} from '../formatting.js';
+import Input from './Input.jsx';
+import Button from '../Button.jsx';
 
 export default function Checkout () {
- const cartCtx = useContext (CartContext);
+  const cartCtx = useContext (CartContext);
 
- const cartTotal = cartCtx.items.reduce (
+  const cartTotal = cartCtx.items.reduce (
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
-    return <Modal>
-        <form action="">
-            <h2>Checkout</h2>
-            <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+  return (
+    <Modal>
+      <form action="">
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format (cartTotal)}</p>
 
-            <Input label = "Full Name" type = "text" id = "full-name"/>
-            <Input label = "E-Mail Address" type ="email" id ="email"/>
-            <Input label = "Street" type ="text" id ="street"/>
-            <div className="control-row">
-            <Input label = "Postal Code" type ="text" id ="postal-code"/>
-            <Input label = "City" type ="text" id ="city"/>
-            </div>
-            <p className="modal-actions">
-                <Button></Button>
-            </p>
-        </form>
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        <p className="modal-actions">
+          <Button type="button"textOnly>Close</Button>
+          <Button textOnly>Submit Order</Button>
+        </p>
+      </form>
     </Modal>
+  );
 }
