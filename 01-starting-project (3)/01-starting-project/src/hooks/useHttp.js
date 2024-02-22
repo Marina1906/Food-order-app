0090025b9274a912ac76440120928b8c5f29acd4
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-22 22:26:35 +0100

    Update useHttp.js

--- 01-starting-project (3)/01-starting-project/src/hooks/useHttp.js
+++ 01-starting-project (3)/01-starting-project/src/hooks/useHttp.js
@@ -1,7 +1,7 @@
 async function sendHttpRequest (url, config) {
  const response = await fetch (url, config);
 
-    const resData =
+    const resData = await response.json ();
 
  if (!response.ok) {
     throw new Error ();
