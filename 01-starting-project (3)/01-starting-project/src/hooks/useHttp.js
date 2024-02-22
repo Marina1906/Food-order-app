5ae01c7d3eafc621c41324232c72b9c0f808c594
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-22 22:25:18 +0100

    Update useHttp.js

--- 01-starting-project (3)/01-starting-project/src/hooks/useHttp.js
+++ 01-starting-project (3)/01-starting-project/src/hooks/useHttp.js
@@ -1,5 +1,11 @@
 async function sendHttpRequest (url, config) {
  const response = await fetch (url, config);
+
+    const resData =
+
+ if (!response.ok) {
+    throw new Error ();
+ }
 }
 
 export default function useHttp() {
