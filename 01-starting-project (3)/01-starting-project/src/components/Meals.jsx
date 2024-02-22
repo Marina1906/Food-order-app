1a8ec8b3bb7b8aecb64d9ba3e7f92e247e1c2c67
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-22 23:14:01 +0100

    Update Meals.jsx

--- 01-starting-project (3)/01-starting-project/src/components/Meals.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/Meals.jsx
@@ -1,26 +1,15 @@
-import { useState, useEffect } from 'react';
+
 import MealItem from './MealItem.jsx';
+import useHttp from '../hooks/useHttp.js';
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+ const {
+  data: loadedMeals,
+   isLoading, 
+  error,
+}= useHttp('http://localhost:3000/meals');
 
-  useEffect(() => {
-    async function fetchMeals() {
-      try {
-        const response = await fetch('http://localhost:3000/meals');
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setLoadedMeals(data); // Update the state with fetched data
-        console.log('Data:', data);
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-      }
-    }
-
-    fetchMeals(); // Call fetchMeals when the component mounts
-  }, []); // Empty dependency array ensures the effect runs only once after mounting
+ 
 
   return (
     <ul id="meals">
