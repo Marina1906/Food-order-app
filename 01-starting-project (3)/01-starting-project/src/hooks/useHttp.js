2496584124f86344d3a38ee8362cd3be01e6582e
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-22 22:41:25 +0100

    Update useHttp.js

--- 01-starting-project (3)/01-starting-project/src/hooks/useHttp.js
+++ 01-starting-project (3)/01-starting-project/src/hooks/useHttp.js
@@ -24,7 +24,7 @@ async function sendRequest () {
 
   const resData =  sendHttpRequest();
 } catch (error) {
-    setError(error.message);
+    setError(error.message || 'Something went wrong!');
 }
 
 }
