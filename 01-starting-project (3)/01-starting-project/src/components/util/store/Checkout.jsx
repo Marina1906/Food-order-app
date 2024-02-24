2f2faa1da70ca56ff5cdb6d757f57fa71e1333ea
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-24 16:25:00 +0100

    Update Checkout.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
@@ -51,7 +51,7 @@ export default function Checkout () {
     )};
 
 let actions = (
-    <>
+    < >
      <Button type="button" textOnly onClick={handleClose}>
         Close
         </Button>
@@ -64,11 +64,20 @@ if (isSending) {
 }
 
 if (data && !error) {
-    return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose} >
+    return (
+    <Modal open={userProgressCtx.progress === 'checkout'} 
+    onClose={handleClose}
+     >
 <h2>Success!</h2>
 <p>Your order was submitted successfully.</p>
-<p>We will get back to you with more details via email within the next few minutes.</p>
+<p>
+    We will get back to you with more details via email within the next few minutes.
+    </p>
+    <p className='modal-actions'>
+       <Button onClick={handleClose}>Okay</Button> 
+    </p>
     </Modal>
+    );
 }
 
   return (
