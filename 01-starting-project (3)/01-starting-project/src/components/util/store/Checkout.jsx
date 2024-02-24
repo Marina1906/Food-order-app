c020b23d558ea550ad4ceb0af3b1b0a0c0e55445
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2024-02-24 15:33:18 +0100

    Update Checkout.jsx

--- 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
+++ 01-starting-project (3)/01-starting-project/src/components/util/store/Checkout.jsx
@@ -7,17 +7,20 @@ import Input from './Input.jsx';
 import Button from '../Button.jsx';
 import UserProgressContext from './UserProgressContext.jsx';
 import useHttp from '../../../hooks/useHttp.js';
-import useHttp from '../../../hooks/useHttp.js';
+
 
 const requestConfig = {
-    method: 'POST'
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    }
 };
 
 export default function Checkout () {
   const cartCtx = useContext (CartContext);
   const userProgressCtx = useContext (UserProgressContext);
 
-  useHttp ('http://localhost:3000/orders', );
+ const {} = useHttp ('http://localhost:3000/orders', );
 
   const cartTotal = cartCtx.items.reduce (
     (totalPrice, item) => totalPrice + item.quantity * item.price,
